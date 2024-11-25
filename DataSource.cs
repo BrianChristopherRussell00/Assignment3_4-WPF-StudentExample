9bcaaada68f15922e6d03bad3f2c355a820c4598
Brian Russell <Brianrussell2017@yahoo.com>
2024-11-25 16:41:38 -0500

    Add project files.

---
+++ DataSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_4_WPF
+{
+    public class DataSource
+    {
+        public static List<Student> Students;
+
+
+        public static List<Student> CreateData()
+        {
+            Students = new List<Student>()
+            { new Student() { StudentId =1, FirstName="Brian", LastName="Russell", Address="123 Nottrue St."},
+             new Student() { StudentId =2, FirstName="Joe", LastName="Joester", Address="124 Nottrue Ave." },
+             new Student() { StudentId =1, FirstName="Jane", LastName="Janeison", Address="125 Nottrue Blvd." }
+            };
+            return Students;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_4_WPF
+{
+    public class Student
+    {
+        public enum MonthofAdmission
+        {
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+        public char Grade { get; set; }
+
+        public MonthofAdmission MonthOfAdmision { get; set; }
+
+        public static List<Student> AddStudent(Student student)
+        {
+            List<Student> Students = new List<Student>();
+            {
+                DataSource.Students.Add(student);
+
+            }
+
+            return Students;
+        }
+        public static List<Student> RemoveStudent(Student student)
+        {
+            List<Student> Students = new List<Student>();
+            {
+                DataSource.Students.Remove(student);
+
+            }
+
+            return Students;
+        }
+    }
+}
